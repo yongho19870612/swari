f3e108b8fdc64172d95a36ca5faae334373cd8a0
kakao_ent <yongho612@gmail.com>
2022-02-20 22:48:24 +0900

    Initial commit

---
+++ src/main/java/com/yuri/swari/SwariApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.swari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwariApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuri/swari/SwariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.swari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
